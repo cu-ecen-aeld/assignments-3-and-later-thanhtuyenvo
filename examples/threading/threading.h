8b935928a717a6001c0eeb25e86935eb139b76bd
tuyenvo <thanhtuyenngt@gmail.com>
2024-06-15 12:45:06 -0700

    rename aesdsocket-start-stop

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -9,7 +9,7 @@
  */
 struct thread_data{
     /*
-     * TODO: add other values your thread will need to manage
+     * TASK: add other values your thread will need to manage
      * into this structure, use this structure to communicate
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
@@ -19,10 +19,11 @@ struct thread_data{
      * Set to true if the thread completed with success, false
      * if an error occurred.
      */
+    bool thread_complete_success;
+    
+    pthread_mutex_t* mutex;
     int wait_to_obtain_ms;
     int wait_to_release_ms;
-    pthread_mutex_t *mutex;
-    bool thread_complete_success;
 };
 
 
