991135f8cd6f1660fb7b235f3a3beba9b9e793cd
tuyenvo <thanhtuyenngt@gmail.com>
2024-06-27 23:00:24 -0700

    Rename the user

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -26,7 +26,7 @@
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
-MODULE_AUTHOR("CamWood7"); /** TODO: fill in your name **/
+MODULE_AUTHOR("ThanhTuyenVo"); /** TODO: fill in your name **/
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
